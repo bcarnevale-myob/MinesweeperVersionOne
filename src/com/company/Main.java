0a89acce4c4e8ceb31da3c0b130147cd58133e9d
bcarnevale-myob <bianca.carnevale@myob.com>
2019-02-20 11:18:44 +1100

    Create fixed board using for loops

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // output the board
+        // attempt one
 
-        String[][] board = {
+        /* String[][] board = {
                 {".", ".", ".", "."},
                 {".", "*", ".", "."},
                 {".", ".", ".", "*"},
@@ -20,6 +20,22 @@ public class Main {
                 System.out.println(data);
             }
 //            System.out.println(Arrays.deepToString(i));
+        } */
+
+        // attempt two
+
+        String[][] board = new String[4][4];
+
+        for (int r=0; r<board.length; r++) {
+            for (int c=0; c<board[r].length; c++) {
+                board[r][c] = ".";
+            }
+        }
+
+        for (String[] i: board) {
+            for (String data: i){
+                System.out.println(data);
+            }
         }
 
     }
