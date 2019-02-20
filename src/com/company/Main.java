a16f68aa71a1e424b97493cf5ba63a3eeeb12196
bcarnevale-myob <bianca.carnevale@myob.com>
2019-02-20 11:49:17 +1100

    Inputted * into the array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,31 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // attempt one
-
-        /* String[][] board = {
-                {".", ".", ".", "."},
-                {".", "*", ".", "."},
-                {".", ".", ".", "*"},
-                {".", ".", "*", "."}
-        };
-
-        for (String[] i: board) {
-            for (String data: i){
-                System.out.println(data);
-            }
-//            System.out.println(Arrays.deepToString(i));
-        } */
-
-        // attempt two
+        // output BoardFixed class
 
         String[][] board = new String[4][4];
 
         for (int r=0; r<board.length; r++) {
             for (int c=0; c<board[r].length; c++) {
+                board[1][1] = "*";
+                board[2][3] = "*";
                 board[r][c] = ".";
             }
         }
+
+        // QUESTION: why doesnt board[3][3] = "*" work??
 
         for (int r=0; r<board.length; r++) {
             for (int c=0; c<board[r].length; c++){
