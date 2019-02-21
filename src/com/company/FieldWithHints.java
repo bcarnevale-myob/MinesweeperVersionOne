2b4deda3ac09c845c179be02fb216c34bc36cc73
bcarnevale-myob <bianca.carnevale@myob.com>
2019-02-21 13:12:44 +1100

    Implemented method to print . or * value to screen

--- src/com/company/FieldWithHints.java
+++ src/com/company/FieldWithHints.java
@@ -2,18 +2,18 @@ package com.company;
 
 public class FieldWithHints {
 
-    private final Square[][] fieldWithHints;
-
-    public Square[][] createFieldWithHints(Square[][] field) {
-        // measure if the square is either side of a MINE
-        
-        // measure if the square is above or below a MINE
-
-        // measure if the square is diagonal to a MINE
-    }
-
-    public FieldWithHints(Square[][] field) {
-        this.fieldWithHints = createFieldWithHints(field);
-    }
+//    private final Square[][] fieldWithHints;
+//
+//    public Square[][] createFieldWithHints(Square[][] field) {
+//        // measure if the square is either side of a MINE
+//
+//        // measure if the square is above or below a MINE
+//
+//        // measure if the square is diagonal to a MINE
+//    }
+//
+//    public FieldWithHints(Square[][] field) {
+//        this.fieldWithHints = createFieldWithHints(field);
+//    }
 
 }
--- src/com/company/Square.java
+++ src/com/company/Square.java
@@ -3,9 +3,23 @@ package com.company;
 public class Square {
 
     public final SquareType squareType;
+    public String value;
 
     public Square(SquareType squareType) {
         this.squareType = squareType;
+    }
+
+    // QUESTION: is the below method necessary? I could not pull out the actual string value * or . to print??
+
+    public String squareValue() throws Exception {
+        if(this.squareType.equals(SquareType.SAFE)) {
+            this.value = ".";
+        } else if(this.squareType.equals(SquareType.MINE)) {
+            this.value = "*";
+        } else {
+            throw new Exception("Valid square types are SAFE and MINE, received: " + value);
+        }
+        return this.value;
     }
 
     Square(String squareType) throws Exception {
