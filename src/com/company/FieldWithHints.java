b19636464b9614d59a59c2125b4dc4164cc0e9fc
bcarnevale-myob <bianca.carnevale@myob.com>
2019-02-22 10:55:52 +1100

    Created method to convert field of squares into field of ints

--- src/com/company/FieldWithHints.java
+++ src/com/company/FieldWithHints.java
@@ -2,24 +2,35 @@ package com.company;
 
 public class FieldWithHints {
 
-    // accessor
+    // similar to field, with int (same methods essentially, diff return types)
 
     // MINE VALUE is in int associated to if that square is a mine
     public static final int MINE_VALUE = -1;
-
-    // similar to field, with int (same methods essentially, diff return types)
-
     private final int[][] fieldWithHints;
 
     private int[][] convertToFieldOfInts(Square[][] field) throws Exception {
         // given field of squares, convert to field of ints
+        int[][] hintField = new int[4][4];
         for (int r = 0; r < field.length; r++) {
             for (int c = 0; c < field[r].length; c++) {
-                if (field[r][c].equals(SquareType.SAFE)) {
-
+                if (field[r][c].toString().equals(".")) {
+                    hintField[r][c] = 0;
+                } else if (field[r][c].toString().equals("*")) {
+                    hintField[r][c] = MINE_VALUE;
+                } else {
+                    throw new Exception("Valid square types are . and *, received: " + field[r][c].toString());
+                }
             }
         }
+        return hintField;
     }
+
+    public boolean inRange(int row, int column) {
+        // needs to check in all directions
+        return (0 <= row && row < fieldWithHints.length) && (0 <= column && column < fieldWithHints.length);
+    }
+
+
 
         // check if the square is SAFE or a MINE
         // if SAFE, change value to 0
@@ -29,11 +40,6 @@ public class FieldWithHints {
         // measure if the square is above or below a MINE
         // measure if the square is diagonal to a MINE
 
-//    private Square[][] fieldWithHints;
-//
-//    public FieldWithHints(Square[][] fieldWithHints) throws Exception {
-//        this.fieldWithHints = createFieldWithHints(fieldWithHints);
-//    }
 //
 //    public boolean inRange(int row, int column) {
 //        // needs to check in all directions
@@ -53,52 +59,3 @@ public class FieldWithHints {
 //        return field;
     }
 
-
-
-
-//    private final Square[][] fieldWithHints;
-
-//    public int changeSquareValue(Square squareType) throws Exception {
-//        int squareValue;
-//        if (squareType.equals(SquareType.SAFE)) {
-//            // squareValue = Integer.parseInt(squareType.squareValue);
-//            squareValue = 0;
-//        } else if (squareType.equals(SquareType.MINE)) {
-//            squareValue = 55;
-//            System.out.print("cool");
-//        } else {
-//            throw new Exception("Valid square types are SAFE and MINE, received: " + squareType);
-//        }
-//        return squareValue;
-//    }
-
-
-//
-//     QUESTION: There must be a way of checking the below method against the SquareType (SAFE or MINE) rather than value?
-//
-//    public Square[][] createFieldWithHints(Square[][] field) throws Exception {
-////         measure if the square is either side of a MINE
-//        for (int r=0; r<field.length; r++) {
-//            for (int c=0; c<field[r].length; c++){
-//                if (field[r - 1][c - 1].squareValue().equals("*") || field[r - 1][c - 1].squareValue().equals("*")) {
-//                    field[r][c] = new Square("1");
-//                }
-//            }
-//        }
-//
-//        for (int r=0; r<field.length; r++) {
-//            for (int c=0; c<field[r].length; c++){
-//                if (field[r][c].equals(SquareType.SAFE)) {
-//
-//                }
-//            }
-//        }
-//
-//        return fieldWithHints;
-//    }
-//
-//    public FieldWithHints(Square[][] field) throws Exception {
-//        this.fieldWithHints = createFieldWithHints(field);
-//    }
-
-}
