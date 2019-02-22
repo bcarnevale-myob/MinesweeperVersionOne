f711f78acdfcf6629f30fbad7818f8a897c9ca9f
bcarnevale-myob <bianca.carnevale@myob.com>
2019-02-22 11:30:41 +1100

    Mapped out a method to add hint values to a field

--- src/com/company/FieldWithHints.java
+++ src/com/company/FieldWithHints.java
@@ -8,6 +8,8 @@ public class FieldWithHints {
     public static final int MINE_VALUE = -1;
     private final int[][] fieldWithHints;
 
+    // check if the square is SAFE or a MINE
+    // if SAFE, change value to 0
     private int[][] convertToFieldOfInts(Square[][] field) throws Exception {
         // given field of squares, convert to field of ints
         int[][] hintField = new int[4][4];
@@ -25,15 +27,42 @@ public class FieldWithHints {
         return hintField;
     }
 
+    // NEED TO DO
+    // get the current square
+    // go around all 8 adjacent squares and check if they are in range
+    // if an adjacent square is a MINE, +1 to the value of the current square
+
+    // all adjacent squares index values, currentSquare [r][c]
+    // rightBottom: [r+1][c+1]
+    // right: [r+1][c]
+    // rightTop: [r+1][c-1]
+    // above: [r][c-1]
+    // leftTop: [r-1][c-1]
+    // left: [r-1][c]
+    // leftBottom: [r-1][c+1]
+    // below: [r][c+1]
+
+    // method to add 1 to a square if a mine is adjacent
+    // if adjacent to a MINE, +1 to the value of the square, cant go above 8
+
+    private int[][] getFieldWithHints(Square[][] field) throws Exception {
+        int[][] hintField = convertToFieldOfInts(field);
+        for (int r = 0; r < hintField.length; r++) {
+            for (int c = 0; c < hintField[r].length; c++) {
+                if (!(hintField[r][c] == -1)) {
+                    // now we have a safe square, check all adjacent squares, +1 if adjacent square is a mine
+                    
+                }
+            }
+        }
+        return hintField;
+    }
+
     public boolean inRange(int row, int column) {
         // needs to check in all directions
         return (0 <= row && row < fieldWithHints.length) && (0 <= column && column < fieldWithHints.length);
     }
 
-
-
-        // check if the square is SAFE or a MINE
-        // if SAFE, change value to 0
         // if adjacent to a MINE, +1 to the value of the square, cant go above 8
 
         // measure if the square is either side of a MINE
